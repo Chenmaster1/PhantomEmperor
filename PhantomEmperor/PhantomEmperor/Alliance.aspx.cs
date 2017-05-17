6144f5f897b4c0db49ecb015d77cc05dc98ea941
Kevin Chen <kcmasterchen@gmail.com>
2017-05-17 14:28:13 +0200

    Allianzen hinzugefügt

---
+++ PhantomEmperor/PhantomEmperor/Alliance.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PhantomEmperor
+{
+    public partial class Alliance : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
